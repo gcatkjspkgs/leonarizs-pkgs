4139abd62475eec89f91aa9eb9780d2a550a6bb1
Gcat101 <gcatmail2@gmail.com>
2023-04-14 14:01:43 +0300

    Update utils

---
+++ README.md
@@ -0,0 +1,12 @@
+# Leonariz's utility scripts
+
+## Functions
+
+```js
+global.leonariz.distance(x, y, z, x2, y2, z2) // Calculate the distance between two points
+globlal.leonariz.randNum(min, max, int) // Get a random number between min and max
+
+global.leonariz.circlePos(event, target, r, particle)
+global.leonariz.rayTrace(event, distance)
+global.leonariz.rayTraceEntities(event, entity)
+```
--- startup_scripts/lib.js
+++ startup_scripts/lib.js
@@ -1,32 +1,25 @@
 onEvent("loaded", e => {
+    global.leonariz.distance = (x, y, z, x2, y2, z2) => {
+        let dis = Math.abs(Math.sqrt(Math.pow(x - x2, 2) + Math.pow(y - y2, 2) + Math.pow(z - z2, 2)))
+        return dis
+    }
+    globlal.leonariz.randNum = (min, max, int) => {
+        let resul
+        if (int) {
+            resul = Math.floor((Math.random() * max) + min)
+        } else {
+            resul = ((Math.random() * max) + min).toFixed(2)
+        }
+        return resul
+    }
+
     global.leonariz.circlePos = (event, target, r, particle) => {
         for (let angle = 0; angle < 6.3; angle += 0.1) {
 
             let x = (r * Math.sin(angle))
             let z = (r * Math.cos(angle))
 
-            event.runCommandSilent(`/particle ${particle} ${target.x + x} ${target.y + 1} ${target.z + z} 0.2 0.2 0.2 0 5`)
-        }
-    }
-
-    // Kubejs's rayTrace is limited to the reach distance, so here's a custom one
-    global.leonariz.rayTraceEntities = (event, entity) => {
-        let start = entity
-        
-        let ray = rayTrace(event, 200)
-
-        let d = distance(start.x, (start.y + event.player.getEyeHeight()), start.z, ray.x, ray.y, ray.z)
-        for (let i = -1; i < d*10; i++) {
-            let delta = i / 10 / d
-            let x = (1 - delta) * start.x + delta * (ray.x)
-            let y = (1 - delta) * (start.y + event.player.getEyeHeight()) + delta * (ray.y)
-            let z = (1 - delta) * start.z + delta * (ray.z)
-            
-            let entity = event.level.getEntitiesWithin(AABB.of(x - 0.2, y - 0.2, z - 0.2, x + 0.2, y + 0.2, z + 0.2))
-            entity = entity.filter(i => !i.isPlayer() && i.isAlive() && i.isLiving())
-            if (entity.length > 0) { 
-                return entity[0]
-            }
+            event.server.runCommandSilent(`/particle ${particle} ${target.x + x} ${target.y + 1} ${target.z + z} 0.2 0.2 0.2 0 5`)
         }
     }
     // It works like the Kubejs's one, but returns the air block if the player is not facing any block
@@ -69,18 +62,24 @@ onEvent("loaded", e => {
             z: PosVec.z
         }
     }
+    // Kubejs's rayTrace is limited to the reach distance, so here's a custom one
+    global.leonariz.rayTraceEntities = (event, entity) => {
+        let start = entity
+        
+        let ray = global.leonariz.rayTrace(event, 200)
 
-    global.leonariz.distance = (x, y, z, x2, y2, z2) => {
-        let dis = Math.abs(Math.sqrt(Math.pow(x - x2, 2) + Math.pow(y - y2, 2) + Math.pow(z - z2, 2)))
-        return dis
-    }
-    globlal.randNum = (min, max, int) => {
-        let resul
-        if (int) {
-            resul = Math.floor((Math.random() * max) + min)
-        } else {
-            resul = ((Math.random() * max) + min).toFixed(2)
+        let d = global.leonariz.distance(start.x, (start.y + event.player.getEyeHeight()), start.z, ray.x, ray.y, ray.z)
+        for (let i = -1; i < d*10; i++) {
+            let delta = i / 10 / d
+            let x = (1 - delta) * start.x + delta * (ray.x)
+            let y = (1 - delta) * (start.y + event.player.getEyeHeight()) + delta * (ray.y)
+            let z = (1 - delta) * start.z + delta * (ray.z)
+            
+            let entity = event.level.getEntitiesWithin(AABB.of(x - 0.2, y - 0.2, z - 0.2, x + 0.2, y + 0.2, z + 0.2))
+            entity = entity.filter(i => !i.isPlayer() && i.isAlive() && i.isLiving())
+            if (entity.length > 0) { 
+                return entity[0]
+            }
         }
-        return resul
     }
 })
